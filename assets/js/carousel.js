488569a91ed32e6b605157950a732e7a80f21e02
Lugas Gilas Angkoro <lugasgilas@gmail.com>
2021-10-18 16:32:29 +0700

    Update besar revisi tampilan
    Update besar revisi tampilan

---
+++ assets/js/carousel.js
@@ -0,0 +1,13 @@
+$('.loop').owlCarousel({
+    center: true,
+    items: 2,
+    dots: false,
+    autoplay: true,
+    loop: true,
+    margin: 10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
