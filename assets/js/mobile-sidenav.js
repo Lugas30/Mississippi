89258be2459cb550af27349324842525c264c8eb
Lugas Gilas Angkoro <lugasgilas@gmail.com>
2021-11-02 17:23:54 +0700

    Update Mobile masih kurang
    Tampilan wishlist dan cart belum sesuai
    tapi bug Jscript sudah fix

---
+++ assets/js/mobile-sidenav.js
@@ -0,0 +1,10 @@
+
+function openMobNav() {
+  document.getElementById("mobileSidenav").style.width = "350px"; 
+}
+
+function closeMobNav() {
+  document.getElementById("mobileSidenav").style.width = "0"; 
+}
+
+   
